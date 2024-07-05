1cd642cb907fd0a138f6e3c46cbf1338e4342c5a
hirotake111 <hirotake111@users.noreply.github.com>
2024-07-06 07:12:49 +0900

    https://www.howtocodeit.com/articles/master-hexagonal-architecture-rust#:~:text=Now%2C%20the%20exciting%20stuff%20%E2%80%93%20the%20implementation%20of%20AuthorRepository%3A

--- src/repository.rs
+++ src/repository.rs
@@ -1,3 +1,9 @@
+use std::str::FromStr;
+
+use anyhow::Context;
+
+use crate::domain::{Author, CreateAuthorError, CreateAuthorRequest};
+
 /// `AuthorRepository` represents a store of author data
 pub trait AuthorRepository {
     /// Persist a new [Author]
@@ -5,5 +11,34 @@ pub trait AuthorRepository {
     /// ## Errors
     ///
     /// - MUST return [CreateAuthorError::Duplicate] if an [Author] with the same name [AuthorName] already exists.
-    fn create_author(&self, req: &CreateAuthorRequest) -> Result<Author, CreateAuthorRequest>;
+    fn create_author(&self, req: &CreateAuthorRequest) -> Result<Author, CreateAuthorError>;
+}
+#[derive(Debug, Clone)]
+pub struct Sqlite {
+    pool: sqlx::SqlitePool,
+}
+
+impl Sqlite {
+    pub async fn new(path: &str) -> anyhow::Result<Sqlite> {
+        let pool = sqlx::SqlitePool::connect_with(
+            sqlx::sqlite::SqliteConnectOptions::from_str(path)
+                .with_context(|| format!("invalid database path: {}", path))?
+                .pragma("foreign_keys", "ON"),
+        )
+        .await
+        .with_context(|| format!("failed to open database at {}", path))?;
+
+        Ok(Sqlite { pool })
+    }
+}
+
+impl AuthorRepository for Sqlite {
+    async fn create_author(&self, req: &CreateAuthorRequest) -> Result<Author, CreateAuthorError> {
+        let mut tx = self
+            .pool
+            .begin()
+            .await
+            .context("failed to start SQLite transaction")?;
+        let author_id = self.save_author(&mut tx, req.name())
+    }
 }
